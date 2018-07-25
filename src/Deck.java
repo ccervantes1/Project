1f4e49d9a7c722e02727068aad9ef3763a364291
Devin <zayas_d91@yahoo.com>
2018-07-24 21:21:54 -0500

    3rd try

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Deck {
+    private List<Flashcard> deckList;
+
+    public Deck() {
+        this.deckList = new ArrayList<>();
+    }
+
+    public void addCard(Flashcard fc) {
+        this.deckList.add(fc);
+    }
+
+    public List<?> getDeck() {
+        return this.deckList;
+    }
+
+    public Flashcard getCard(int i) {
+        return this.deckList.get(i);
+    }
+
+    public List<?> shuffle() {
+        ArrayList<Flashcard> shuffledDeck = new ArrayList<Flashcard>(this.deckList);
+        Collections.shuffle(shuffledDeck, new Random((long) Math.random()));
+        return shuffledDeck;
+    }
 }
