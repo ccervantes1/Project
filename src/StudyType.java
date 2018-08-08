3c2b36671b78ae639b05855046665457bb53d8e7
Devin <zayas_d91@yahoo.com>
2018-08-08 12:06:00 -0500

    Final commit2

--- src/StudyType.java
+++ src/StudyType.java
@@ -2,40 +2,38 @@ import java.util.LinkedList;
 import java.util.Collections;
 
 public class StudyType {
-    private Flashcard input1;
-    private LinkedList<Card> cardDeck;
+    private FlashCardBuilder input1;
+    private LinkedList<FlashCardBuilder> cardDeck;
     private int time;
 
     private static final int EASY = 20;
     private static final int MODERATE = 10;
     private static final int DIFFICULT= 5;
     public StudyType() {
-        input1 = new Flashcard();
-        cardDeck = input1.getCards();
+        input1 = new FlashCardBuilder();
+
         time = 0;
     }
 
     public void setEasy() {
-        Card card = cardDeck.removeFirst();
+        FlashCardBuilder card = cardDeck.removeFirst();
         card.setInterval(EASY);
         cardDeck.add(EASY, card);
     }
 
     public void setModerate() {
-        Card card = cardDeck.removeFirst();
+        FlashCardBuilder card = cardDeck.removeFirst();
         card.setInterval(MODERATE);
         cardDeck.add(MODERATE, card);
     }
 
     public void setDifficult() {
-        Card card = cardDeck.removeFirst();
+        FlashCardBuilder card = cardDeck.removeFirst();
         card.setInterval(DIFFICULT);
         cardDeck.add(DIFFICULT, card);
     }
 
-    public Card getNextCard() {
-        return cardDeck.getFirst();
-    }
+
 
     public int getTime() {
         return time;
@@ -45,4 +43,4 @@ public class StudyType {
 
     }
 
-    }
+}
